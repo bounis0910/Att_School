3a2d9ba939fd301e9bf874f77a795ce0898cd04e
OunisDev <ounis.belgacem@outlook.com>
2025-11-27 09:40:34 +0300

    Att_school_V3

--- app.py
+++ app.py
@@ -1707,4 +1707,6 @@ def admin_export_pdf():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #app.run(host='10.108.97.179', port=5000)
+    #app.run(host='10.108.97.179', port=5312)
+    #app.run(debug=True, host='127.0.0.1', port=5312)
+
